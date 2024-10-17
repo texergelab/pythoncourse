5d05cf74ed05d7f314fdb2e2ddafc927ea7a0c30
texergelab <texergelab@gmail.com>
2024-10-17 16:22:20 +0000

    added 112

--- menu.py
+++ menu.py
@@ -4,26 +4,47 @@ menu_item_prices = [100,200,300]
 
 def show_menu():
 
-    for number in range(0,3):
+    for number in range(0,len(menu_items)):
         print(f"{menu_items[number]} : {menu_item_prices[number]} /-")
 
 def add_menu():
+    show_menu()
     item_name = input("Enter menu item to add:  ")
     menu_items.append(item_name)
     item_price = input("Enter menu price to add:  ")
     menu_item_prices.append(item_price)
+    show_menu()
 
-def 
-
+def remove_menu():
+    show_menu()
+    item_name = input("Enter menu name which you want to remove")
+    index=menu_items.index(item_name)
+    menu_items.remove(item_name)
+    del menu_item_prices[index]
+    # print(menu_item_prices)
+    # print(menu_items)
+    show_menu()
 
 def take_order():
     order = input("Enter your Order : ")
     if order == "Burger":
         print("Please pay 100 /-")
 
+#add_menu()
+#remove_menu()
+# show_menu()
+# take_order()
+
+#user_choice=input("Do you want show menu, remove menu or add menu? ")
+
+while(True):
+    user_choice = int(input("Press \n 1 to show menu :  \n 2 to remove menu :  \n 3 to add menu :  \n 0 to exit :  "))
     
-
-show_menu()
-take_order()
-
-
+    if user_choice == 1:
+        show_menu()
+    if user_choice == 2:
+        remove_menu()
+    if user_choice == 3:
+        add_menu()
+    if user_choice == 0:
+        break
---
+++ test.py
@@ -0,0 +1,8 @@
+list1 = ["A","b","c"]
+list2=[10,20,30]
+list1.remove("b")
+
+print(list1)
+# del list2[1]
+
+print(list2)
